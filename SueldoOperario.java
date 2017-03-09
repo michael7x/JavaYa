b9ed9c551bddf78d60be6c6359fad49acd980414
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-03-09 16:57:15 -0300

    Create SueldoOperario.java

---
+++ SueldoOperario.java
@@ -0,0 +1,27 @@
+/* https://www.tutorialesprogramacionya.com/javaya/detalleconcepto.php?punto=4&codigo=77&inicio=0 */
+
+package scriptsJava;
+import java.util.Scanner;
+
+public class SueldoOperario {
+	
+	public static void main(String[] ar){
+		Scanner teclado = new Scanner(System.in);
+		
+		int horasTrabajadas;
+		float costoHora;
+		float sueldo;
+		
+		System.out.print("Ingrese la cantidad de horas trabajadas por el empleado: ");
+		horasTrabajadas = teclado.nextInt();
+		
+		System.out.print("Ingrese el valor de la hora: ");
+		costoHora = teclado.nextFloat();
+		
+		sueldo = horasTrabajadas * costoHora;
+		
+		System.out.print("El empleado debe cobrar: ");
+		System.out.print(sueldo);
+	}
+
+}
