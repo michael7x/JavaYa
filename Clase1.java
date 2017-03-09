32a3202dbb859bbf5d116c47e76fe3b4dea77bee
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-03-09 16:55:34 -0300

    Create Clase1.java

---
+++ Clase1.java
@@ -0,0 +1,11 @@
+/* de https://www.tutorialesprogramacionya.com/javaya/detalleconcepto.php?punto=3&codigo=76&inicio=0 */
+
+package scriptsJava;
+
+public class Clase1 {
+	
+	public static void main(String[] parametro){
+		System.out.println("Hola Mundo Java");
+	}
+
+}
