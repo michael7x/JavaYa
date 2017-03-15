ca571674492c5a1e56c4c55d1fb5558e7e271efe
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-03-15 17:50:16 -0300

    Create Alturas.java

---
+++ capitulo10/Alturas.java
@@ -0,0 +1,21 @@
+//Se ingresan un conjunto de n alturas de personas por teclado. Mostrar la altura promedio de las personas. 
+//De https://www.tutorialesprogramacionya.com/javaya/detalleconcepto.php?punto=10&codigo=83&inicio=0
+
+package capitulo10;
+import java.util.Scanner;
+
+public class Alturas {
+	
+	public static void main(String[] args){
+		
+		Scanner teclado = new Scanner(System.in);
+		int pacientes = 0;
+		int altura = 0;
+		int alturatotal = 0;
+		
+		
+		
+				
+	}	
+
+}
