6f27b4f24d94235d8e07965cdcb68788f0ea3e6f
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-04-11 10:19:05 -0300

    Create TablaDeMultiplicarCinco.java

---
+++ capitulo11/TablaDeMultiplicarCinco.java
@@ -0,0 +1,20 @@
+/* Ej3: 
+*Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50) 
+*/
+// De: https://www.tutorialesprogramacionya.com/javaya/detalleconcepto.php?punto=11&codigo=84&inicio=0
+
+
+
+package capitulo11;
+
+public class TablaDeMultiplicarCinco {
+	
+	public static void main(String[] arg){
+		
+		for(int i = 0; i <= 10; i++ ){
+			System.out.println(i + " X " + 5 + " = " + (i*5));
+		}
+		
+	}
+
+}
