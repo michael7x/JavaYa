14419e1bf89d70caf542ed5ba57ab9db7af3d101
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-04-11 10:23:45 -0300

    Create EstructuraRepetitivaFor1.java

---
+++ capitulo11/EstructuraRepetitivaFor1.java
@@ -0,0 +1,18 @@
+/*
+ * Realizar un programa que imprima en pantalla los números del 1 al 100.
+ */
+
+// De: https://www.tutorialesprogramacionya.com/javaya/detalleconcepto.php?punto=11&codigo=84&inicio=0
+
+package capitulo11;
+
+public class EstructuraRepetitivaFor1 {
+	public static void main(String[] arg){
+		int f;
+		for(f=1;f<=100;f++){
+			System.out.print(f);
+			System.out.print("-");
+		}
+	}
+
+}
