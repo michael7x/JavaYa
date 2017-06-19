0b741c19a45b40e3d6ad7ff25ca8a0dec73571c4
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-06-19 18:16:31 -0300

    Create Cliente.java

---
+++ capitulo28/Cliente.java
@@ -0,0 +1,30 @@
+package capitulo28;
+
+public class Cliente {
+	private String nombre;
+	private int monto;
+	
+	public Cliente(String nom){
+		nombre = nom;
+		monto = 0;		
+	}
+
+	public void depositar(int m){
+		monto = monto + m;		
+	}
+	
+	public void exraer(int m){
+		monto = monto - m;
+	}
+	
+	public int retornarMonto(){
+		return monto;
+	}
+	
+	public void imprimir(){
+		System.out.println(nombre + " tiene depositado la suma de " + monto);
+	}
+	
+	
+	
+}
