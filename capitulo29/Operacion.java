e74dd6cdbd71c0eb950c13026c95e8170f2f69a6
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-06-22 11:24:44 -0300

    Create Operacion.java

---
+++ capitulo29/Operacion.java
@@ -0,0 +1,29 @@
+package capitulo29;
+
+import java.util.Scanner;
+
+public class Operacion {
+	protected Scanner teclado;
+	protected int valor1;
+	protected int valor2;
+	protected int resultado;
+	
+	public Operacion(){
+		teclado = new Scanner(System.in);
+	}
+	
+	public void cargar1(){
+		System.out.println("Ingrese el primer valor: ");
+		valor1 = teclado.nextInt();
+	}
+	
+	public void cargar2(){
+		System.out.println("Ingrese el segundo valor: ");
+		valor2 = teclado.nextInt();
+	}
+	
+	public void mostrarResultado(){
+		System.out.println(resultado);
+	}
+
+}
