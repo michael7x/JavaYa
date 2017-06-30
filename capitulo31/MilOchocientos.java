436bc3288c23fb2384e2c70f2aa738a2dc43561b
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-06-30 11:34:59 -0300

    Create MilOchocientos.java

---
+++ capitulo31/MilOchocientos.java
@@ -0,0 +1,17 @@
+package capitulo31;
+
+import javax.swing.*;
+
+public class MilOchocientos extends JFrame{
+	public MilOchocientos(){
+		setLayout(null);
+	}	
+	
+	public static void main(String[] arg){
+		MilOchocientos prueba = new MilOchocientos();
+		prueba.setBounds(10,10,1024,800);
+		prueba.setResizable(true);
+		prueba.setVisible(true);
+	}
+
+}
