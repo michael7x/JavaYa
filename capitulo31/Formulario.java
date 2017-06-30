af6ae52e49d3ef3cdace4e4801440af5f9727f84
Joaquín Chemile <jchemile@users.noreply.github.com>
2017-06-30 11:35:23 -0300

    Create Formulario.java

---
+++ capitulo31/Formulario.java
@@ -0,0 +1,17 @@
+package capitulo31;
+
+import javax.swing.*;
+
+public class Formulario extends JFrame{
+	
+	public Formulario(){
+		setLayout(null);
+	}
+	
+	public static void main(String[] arg){
+		Formulario pruebaformulario = new Formulario();
+		pruebaformulario.setBounds(10,20,400,300);
+		pruebaformulario.setVisible(true);
+	}	
+	
+}
